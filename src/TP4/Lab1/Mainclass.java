18f27830e52472ce7320667c7b19f2c296cd28fd
GonzaloBustos8 <163307288+GonzaloBustos8@users.noreply.github.com>
2024-03-29 16:29:12 -0300

    Initial commit

---
+++ src/TP4/Lab1/Mainclass.java
@@ -0,0 +1,45 @@
+package TP4.Lab1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author busto
+ */
+public class Mainclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+/** Armar grupo de no más de 4 integrantes para resolver el siguiente ejercicio.
+Utilizar GitHub, subir proyecto a repositorio y agregue compañeros como colaboradores.
+Al entregar la tarea, detalle: apellido, nombre y nro de documento de todos
+los integrantes del equipo.
+
+Ejercicio 2:
+
+Crea una aplicación GUI en Java que simule un convertidor de unidades de
+temperatura. La interfaz debe contener los siguientes elementos:
+
+Un campo de texto (JTextField) para que el usuario pueda ingresar la temperatura en grados Celsius.
+Un botón (JButton) con el texto "Convertir".
+Cuando el usuario ingrese la temperatura en grados Celsius y haga clic en el botón
+"Convertir", la aplicación debe realizar la conversión de Celsius a Fahrenheit y mostrar
+el resultado en un cuadro de diálogo emergente.
+
+La fórmula para convertir de grados Celsius a Fahrenheit es: Fahrenheit = Celsius * 9/5 + 32.
+
+Recuerda utilizar la biblioteca de componentes gráficos de Java (Swing) para crear la interfaz y gestionar el evento del botón.
+
+Subir proyecto a GitHub y enviar link al repositorio. */
+    }
+    
+}
